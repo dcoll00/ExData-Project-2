7595626a525afc07cc0e485fd52ade64b8bda1e9
dcoll00 <dc.1.drakec@cox.net>
2014-08-21 21:41:52 -0700

    This is loading the first two plots of the assigment

---
+++ plot2.R
@@ -0,0 +1,23 @@
+## Project 2 Second question
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+#SCC <- readRDS("Source_Classification_Code.rds")
+#SumSCC <- readRDS("summarySCC_PM25.rds")
+
+# subset the data for the specific years
+NEI_Subset <- NEI[NEI$year %in% c(1999,2002,2005,2008) & NEI$fips == "24510",]
+
+# summarize the data
+cdata <- ddply(NEI_Subset, c("year"), summarise, N = sum(Emissions))
+
+#open the output file
+png('proj_2_rplot2.png', width = 480, height = 480)
+
+#plot the data
+plot(cdata$year,cdata$N,type="l",xlab="Year", ylab="Total Emissions in tons",xaxt="n")
+axis(1,at=cdata$year)
+title(main="Did total emissions from PM2.5 decreased in Baltimore City")
+
+# reset the device
+dev.off()
+
